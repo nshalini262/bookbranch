b4df518a2ae4c724fd64675dfba15c5cbeb0e833
Shalini Nannapaneni <nshalini262@gmail.com>
2025-04-16 19:07:11 -0400

    visualizing dataset intial

--- main.py
+++ main.py
@@ -1,17 +1,3 @@
+import pandas as pd
+books_df = pd.read_csv("GoodReads_100k_books.csv")
 
-# filtered_df = df.loc[df['isbn'] < 1906863482, ['author']]
-
-import kagglehub
-from kagglehub import KaggleDatasetAdapter
-
-# Set the path to the file you'd like to load
-file_path = "GoodReads_100k_books.csv"
-
-# Load the latest version
-df = kagglehub.load_dataset(
-  KaggleDatasetAdapter.PANDAS,
-  "mdhamani/goodreads-books-100k",
-  file_path,
-)
-
-print("First 5 records:", df.head())
